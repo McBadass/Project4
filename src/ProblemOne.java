3f4f60ab491695920785aab4baf7167d89bc1144
McBadass <username@users.noreply.github.com>
2014-09-25 12:40:24 -0600

    Initial commit

---
+++ src/ProblemOne.java
@@ -0,0 +1,22 @@
+// Class:			ProblemOne.java
+//
+// Author:			Taylor Vories
+//
+// Class:			CS 2050
+//
+// Description:		Recursive Method that takes an int argument k and returns,
+//                  as a double, the sum of the first k terms of the series.
+//
+// Files:			
+//-----------------------------------------------------------------------
+public class ProblemOne {
+	private double seriesSum;
+
+	public ProblemOne() {
+		//TODO: Need a constructor?
+	}
+
+	public double process(int k) {
+		
+	}
+}
